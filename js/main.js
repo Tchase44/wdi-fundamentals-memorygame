3c6f081b6814504d11aa8d1faf57545125edb98f
Tchase44 <Tchase4544@gmail.com>
2017-04-24 10:20:00 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("user selected "+cardOne);
+
+var card = ["queen","queen","king","king"];
+var cardsInPlay = [];
+
+var cardOne = card[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = card[1];
+cardsInPlay.push(cardTwo);
+
+console.log("user flipped "+cardsInPlay[0]);
+console.log("user flipped "+cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You Found a Match!!!");
+	}else{
+		alert("Sorry try again");
+	};
+};
